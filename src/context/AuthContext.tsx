88f703d78bc9c5beb9aec8675188e30ac5017658
Yusuf Celal Çelik <yusufcelalcelik@Yusufs-MacBook-Pro.local>
2026-07-14 23:51:29 +0300

    fix: Context oluşturldu ve hataları çözüldü, layout d alogin kontorlü yapıalcak

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,25 @@
+import { createContext, useContext, useState } from "react";
+
+type AuthContextType = {
+    isLogged: boolean;
+    setIsLogged: (value: boolean) => void;
+};
+
+
+const AuthContext = createContext<AuthContextType | null>(null);
+
+
+export function AuthProvider({ children }: { children: ReactNode }) {
+
+
+
+    const [isLogged, setIsLogged] = useState(false);
+
+    return <AuthContext.Provider value={{ isLogged, setIsLogged }}>{children}</AuthContext.Provider>
+
+}
+export function useAuth() {
+
+    return useContext(AuthContext);
+
+}
