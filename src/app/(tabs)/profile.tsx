2a3f8c199e95e224aeda915320853fa7466bc2c3
Yusuf Celal Çelik <info@yusufcelalcelik.com>
2026-07-18 21:51:10 +0300

    feat: profile sekmesi oluşturuldı

---
+++ src/app/(tabs)/profile.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function ProfileScreen(){
+
+
+    return(
+        <SafeAreaView>
+            
+        </SafeAreaView>
+    )
+}
