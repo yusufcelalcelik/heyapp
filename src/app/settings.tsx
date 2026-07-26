409d36a5b349a94e4a8fee9b928fb051f8361187
Yusuf Celal Çelik <info@yusufcelalcelik.com>
2026-07-26 13:16:03 +0300

    feat: Settigns sayfasına logout tuşu eklendi

--- src/app/settings.tsx
+++ src/app/settings.tsx
@@ -1,8 +1,11 @@
 import { Pressable, View, Text } from "react-native";
 import { useAuth } from "@/context/AuthContext";
+import { Header } from "@/components/Header";
+import { useTheme } from '@/hooks/use-theme';
 
 export default function Settings() {
     const { logout } = useAuth();
+    const theme = useTheme();
 
     const handleLogout = () => {
         logout();
@@ -10,6 +13,7 @@ export default function Settings() {
 
     return (
         <View>
+            <Header showBack={true} />
             <Pressable onPress={handleLogout}>
                 <Text>ÇIKIŞ YAP</Text>
             </Pressable>
