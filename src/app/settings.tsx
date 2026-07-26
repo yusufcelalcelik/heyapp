6b96d569fe4a588f75b1c76f6bf31c9867e51a3a
Yusuf Celal Çelik <info@yusufcelalcelik.com>
2026-07-26 14:58:23 +0300

    feat: handleLogout için onay alerti ekelndi

--- src/app/settings.tsx
+++ src/app/settings.tsx
@@ -1,4 +1,4 @@
-import { Pressable, View, Text } from "react-native";
+import { Pressable, View, Text,Alert } from "react-native";
 import { useAuth } from "@/context/AuthContext";
 import { Header } from "@/components/Header";
 import { useTheme } from '@/hooks/use-theme';
@@ -8,7 +8,22 @@ export default function Settings() {
     const theme = useTheme();
 
     const handleLogout = () => {
-        logout();
+        Alert.alert(
+        "Çıkış Yap",
+        "Çıkmak istediğinizden emin misiniz?",
+        [
+            { 
+                text: "İptal", 
+                onPress: () => {}, 
+                style: "cancel" 
+            },
+            { 
+                text: "Çıkış", 
+                onPress: () => logout(), 
+                style: "destructive" 
+            }
+        ]
+    );
     };
 
     return (
