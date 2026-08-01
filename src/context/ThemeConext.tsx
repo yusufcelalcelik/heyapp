bdaafc5575aaf1313443fa05f59e77860e0d3248
Yusuf Celal Çelik <info@yusufcelalcelik.com>
2026-08-01 15:36:20 +0300

    feat: ThemeContext Eklendi

---
+++ src/context/ThemeConext.tsx
@@ -0,0 +1,57 @@
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useColorScheme } from "@/hooks/use-color-scheme";
+
+// Tip tanımlamaları 
+type ThemeContextType = {
+    themeMode: ThemeMode;
+    setThemeMode: (mode: ThemeMode) => void;
+    resolvedTheme: 'light' | 'dark';
+};
+
+const VALID_MODES = ['light', 'dark', 'system'] as const;
+type ThemeMode = typeof VALID_MODES[number]
+
+const ThemeContext = createContext<ThemeContextType | null>(null);
+
+const STORAGE_KEY = "theme_mode";
+
+export function ThemeModeProvider({ children }: { children: ReactNode }) {
+    const systemScheme = useColorScheme();
+    const [themeMode, setThemeModeState] = useState<ThemeMode>('system');
+
+    // buraya AsyncStorage okuma gelecek (adım 3)
+    useEffect(() => {
+        AsyncStorage.getItem(STORAGE_KEY).then((saved) => {
+            if (VALID_MODES.includes(saved as ThemeMode)) {
+                setThemeModeState(saved as ThemeMode);
+            }
+        });
+
+    }
+    )
+
+    // buraya setThemeMode fonksiyonu gelecek (adım 4)
+    const setThemeMode = (mode: ThemeMode) => {
+        setThemeModeState(mode);
+        AsyncStorage.setItem(STORAGE_KEY, mode);
+    }
+
+    // buraya resolvedTheme hesaplaması gelecek (adım 5)
+    const resolvedTheme: 'light' | 'dark' =
+        themeMode === 'system'
+            ? (systemScheme === 'dark' ? 'dark' : 'light')
+            : themeMode;
+
+    return <ThemeContext.Provider value={{ themeMode, setThemeMode, resolvedTheme }}>{children}</ThemeContext.Provider>;
+}
+
+export function useTheme() {
+    const context = useContext(ThemeContext)
+
+    if (context == null) throw new Error("useTheme, ThemeProvider içinde kullanılmalı");
+
+    return context;
+
+};
+
